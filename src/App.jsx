b85938e2792db4cd5d70b14ef26772b79737f196
John Assefa <johnassefaforbussines@gmail.com>
2024-10-30 12:18:19 -0700

    seting up environmern

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Environment background={false} files={'assets/textures/envmap.hdr'} />
+      <PerspectiveCamera makeDefault position={[0, 10, 10]} />
+      <OrbitControls target0={[0, 0, 0]}  />
     </>
   )
 }

90f835a98a0e752421c827c368876944f009164c
John Assefa <johnassefaforbussines@gmail.com>
2024-10-30 13:38:10 -0700

    creating the landscape

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import { Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import { SphereEnv } from './SphereEnv'
+import { Landscape } from './Landscape'
 
 function App() {
 
@@ -11,6 +12,22 @@ function App() {
       <Environment background={false} files={'assets/textures/envmap.hdr'} />
       <PerspectiveCamera makeDefault position={[0, 10, 10]} />
       <OrbitControls target0={[0, 0, 0]}  />
+      <Landscape />
+
+      <directionalLight 
+      castShadow
+      color={'#ffffff'} 
+      shadow-bias={-0.0005}
+      shadow-mapSize-width={1024}
+      shadow-mapSize-height={1024}
+      shadow-camera-far={20}
+      shadow-camera-left={-6.2}
+      shadow-camera-right={6.2}
+      shadow-camera-top={6}
+      shadow-camera-bottom={-6}
+      shadow-camera-near={0.1}
+      position={[10, 5, 4]} 
+      intensity={2} />
     </>
   )
 }
---
+++ src/Landscape.jsx
@@ -0,0 +1,50 @@
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+
+export function Landscape(props) {
+  const { nodes, materials } = useGLTF('assets/models/scene.glb')
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.landscape_gltf.geometry}
+        material={materials['Material.009']}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.landscape_borders.geometry}
+        material={materials['Material.010']}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.trees_light.geometry}
+        material={materials['Material.008']}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.walls.geometry}
+        material={nodes.walls.material}
+        position={[0, 3, -5.109]}
+        rotation={[Math.PI / 2, 0, 0]}
+        scale={[5, 1, 3]}
+      />
+      <mesh castShadow receiveShadow geometry={nodes.water.geometry} material={materials.Water} />
+      <mesh castShadow receiveShadow geometry={nodes.water1.geometry} material={materials.Water} />
+      <mesh castShadow receiveShadow geometry={nodes.water2.geometry} material={materials.Water} />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.lights.geometry}
+        material={materials['Material.001']}
+      />
+    </group>
+  )
+}
+
+useGLTF.preload('/scene.glb')
+
+
